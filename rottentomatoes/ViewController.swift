cb170b3891a2dae6b3c75a13852cacb37f89320c
Jesse Smith <sosaucily@gmail.com>
2014-09-14 19:10:16 -0700

    Fetch data from rotten tomatoes API via AFNetworking lib

--- rottentomatoes/ViewController.swift
+++ rottentomatoes/ViewController.swift
@@ -8,18 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet weak var movieTableView: UITableView!
+    var moviesArray: NSArray?
+    
+    let RottenTomatoesAPIKey = "3ep56bczb367ku9hruf7xpdq"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        self.getMovieData()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return (moviesArray != nil) ?
+            moviesArray!.count :
+            0
     }
-
-
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = movieTableView.dequeueReusableCellWithIdentifier("com.codepath.rottentomatoes.moviecell") as MovieTableViewCell
+        let movieDictionary = self.moviesArray![indexPath.row] as NSDictionary
+        cell.titleLabel.text = movieDictionary["title"] as NSString
+        return cell
+    }
+    
+    func getMovieData() {
+        let RottenTomatoesURLString = "http://api.rottentomatoes.com/api/public/v1.0/lists/dvds/top_rentals.json?apikey=" + RottenTomatoesAPIKey
+        
+        let manager = AFHTTPRequestOperationManager()
+        
+        manager.GET(RottenTomatoesURLString, parameters: nil,
+            success: self.updateViewWithMovies,
+            failure: self.fetchMoviesError)
+        
+        let request = NSMutableURLRequest(URL: NSURL.URLWithString(RottenTomatoesURLString))
+        
+    }
+    
+    func updateViewWithMovies(operation: AFHTTPRequestOperation!,
+        responseObject: AnyObject!) -> Void {
+            self.moviesArray = responseObject["movies"] as? NSArray
+            self.movieTableView.reloadData()
+    }
+    
+    func fetchMoviesError(operation: AFHTTPRequestOperation!,
+        error: NSError!) -> Void {
+            println("Error: " + error.localizedDescription)
+    }
 }
 
